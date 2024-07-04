251fe35de21323d24f7f32d848a08bcacffc2d64
edu <168421130+ebrionesm@users.noreply.github.com>
2024-07-04 13:54:33 +0200

    corregidos

--- Tema1/Estructura/Ex2/pizzeria.sql
+++ Tema1/Estructura/Ex2/pizzeria.sql
@@ -43,70 +43,47 @@ CREATE TABLE pedido (
     fecha_entrega DATE,
     hora_entrega TIME,
     id_cliente INT NOT NULL,
+    id_tienda INT NOT NULL,
     id_empleado INT,
     PRIMARY KEY (id_pedido),
 	FOREIGN KEY(id_cliente) REFERENCES cliente(id_cliente),
-    FOREIGN KEY(id_empleado) REFERENCES pizzeria.empleado(id_empleado)
+    FOREIGN KEY(id_tienda) REFERENCES pizzeria.tienda(id_tienda),
+    FOREIGN KEY(id_empleado) REFERENCES empleado(id_empleado)
 );
 
-
+CREATE TABLE categoria (
+	id_categoria INT UNSIGNED PRIMARY KEY,
+    nombre VARCHAR(30) NOT NULL
+);
 
 CREATE TABLE producto (
-	id_producto INT UNSIGNED PRIMARY KEY
+	id_producto INT UNSIGNED PRIMARY KEY,
+    tipo ENUM('pizza', 'hamburguesa', 'bebida') NOT NULL,
+	nombre VARCHAR(30) NOT NULL,
+    descripcion VARCHAR(200) NOT NULL,
+    imagen VARCHAR(100) NOT NULL,
+    precio DECIMAL(2) NOT NULL,
+    id_categoria INT UNSIGNED,
+    FOREIGN KEY (id_categoria) REFERENCES categoria(id_categoria)
 );
 
 CREATE TABLE pedido_tiene_producto (
 	id_pedido INT UNSIGNED NOT NULL,
     id_producto INT UNSIGNED NOT NULL,
+    cantidad_producto INT NOT NULL,
     PRIMARY KEY(id_pedido, id_producto),
     FOREIGN KEY (id_pedido) REFERENCES pedido(id_pedido),
     FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
 );
 
-CREATE TABLE pizza (
-	id_producto INT UNSIGNED PRIMARY KEY,
-    tipo VARCHAR(20) DEFAULT 'pizza' NOT NULL,
-	nombre VARCHAR(30) NOT NULL,
-    descripcion VARCHAR(200) NOT NULL,
-    imagen VARCHAR(100) NOT NULL,
-    precio DECIMAL(2) NOT NULL,
-    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-);
 
-CREATE TABLE hamburguesa (
-	id_producto INT UNSIGNED PRIMARY KEY,
-    tipo VARCHAR(20) DEFAULT 'hamburguesa' NOT NULL,
-	nombre VARCHAR(30) NOT NULL,
-    descripcion VARCHAR(200) NOT NULL,
-    imagen VARCHAR(100) NOT NULL,
-    precio DECIMAL(2) NOT NULL,
-    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-);
-
-CREATE TABLE bebida (
-	id_producto INT UNSIGNED PRIMARY KEY,
-    tipo VARCHAR(20) DEFAULT 'bebida' NOT NULL,
-	nombre VARCHAR(30) NOT NULL,
-    descripcion VARCHAR(200) NOT NULL,
-    imagen VARCHAR(100) NOT NULL,
-    precio DECIMAL(2) NOT NULL,
-    FOREIGN KEY (id_producto) REFERENCES producto(id_producto)
-);
-
-CREATE TABLE categoria (
-	id_categoria INT UNSIGNED PRIMARY KEY,
-    nombre VARCHAR(30) NOT NULL,
-    id_pizza INT UNSIGNED NOT NULL,
-    FOREIGN KEY (id_pizza) REFERENCES pizza(id_producto)
-);
 
 
 INSERT INTO cliente VALUE (1, 'Nombre1', 'Apellido1', 'Apellido2', 'Calle1', 90000, 'Localidad1', 'Provincia1', 654321987);
 INSERT INTO tienda VALUE (1, 'Calle2', 90000, 'Localidad1', 'Provincia1');
 INSERT INTO empleado VALUE (1, 'Empleado1', 'ApellidoEmpleado1', 'ApellidoEmpleado2', '12345678A', 'reparto', 1);
-INSERT INTO pedido VALUE(1, '2024/06/04', '12:00:00', 'domicilio', 0, 0, 1, 2, '2024/06/04', '12:30:00', 1, 1);
-INSERT INTO producto VALUE(1);
-INSERT INTO producto VALUE(2);
-INSERT INTO pedido_tiene_producto VALUE(1, 2);
-INSERT INTO pizza VALUE(1, 'pizza' , 'pizza1', 'descripcion_pizza1', 'imagen.url', 9.99);
-INSERT INTO bebida VALUE(2, 'bebida', 'bebida1', 'descripcion_bebida1', 'imagen_bebida.url', 2);
+INSERT INTO pedido VALUE(1, '2024/06/04', '12:00:00', 'domicilio', 0, 0, 1, 2, '2024/06/04', '12:30:00', 1, 1, 1);
+INSERT INTO categoria VALUE(1, 'categoria1');
+INSERT INTO producto VALUE(1, 'pizza' , 'pizza1', 'descripcion_pizza1', 'imagen.url', 9.99, 1);
+INSERT INTO producto VALUE(2, 'bebida', 'bebida1', 'descripcion_bebida1', 'imagen_bebida.url', 2, NULL);
+INSERT INTO pedido_tiene_producto VALUE(1, 2, 1);
